20d9411bcd91fddbd625134861559365c8ea43fb
Ramya-Ravisankar <rb756@njit.edu>
2024-03-06 03:01:09 -0500

    dunder init changes for commands/add

--- app/commands/add/__init__.py
+++ app/commands/add/__init__.py
@@ -1,6 +1,18 @@
-import sys
 from app.commands import Command
+from calculator import Calculator
+from app.utils.validation import validate_decimal_input
 
 class AddCommand(Command):
+    '''A command class to perform addition.'''
+
     def execute(self):
-        print("Hello Ramya")
+        '''
+        Execute the AddCommand.
+
+        This method prompts the user to enter two numbers and performs addition.
+        '''
+        num1 = validate_decimal_input("Enter the first number: ")
+        num2 = validate_decimal_input("Enter the second number: ")
+        result = Calculator.add(num1, num2)
+        print(f"The result of {num1} + {num2} is: {result}")
+        return result # for test_add_command.py
