7d2b50d0547320e29f268bc8bb4b0717168c39e1
Ramya-Ravisankar <rb756@njit.edu>
2024-03-14 14:52:45 -0400

    app/plugins/add/__init__.py file changes

--- app/plugins/add/__init__.py
+++ app/plugins/add/__init__.py
@@ -1,7 +1,8 @@
 '''app/plugins/add/__init__.py'''
 from app.commands import Command
-from calculator import Calculator
+from app.calculator import Calculator
 from app.utils.validation import validate_decimal_input
+import logging
 
 class AddCommand(Command):
     '''A command class to perform addition.'''
@@ -12,8 +13,11 @@ class AddCommand(Command):
 
         This method prompts the user to enter two numbers and performs addition.
         '''
+        logging.info("Command 'add' from plugin 'menu' selected.")
         num1 = validate_decimal_input("Enter the first number: ")
         num2 = validate_decimal_input("Enter the second number: ")
+
+        logging.info("Performing addition...")
         result = Calculator.add(num1, num2)
         print(f"The result of {num1} + {num2} is: {result}")
         return result # for test_add_command.py
