511b3db0d666d4a157dabb405b975cb72aee9c3d
Ramya-Ravisankar <rb756@njit.edu>
2024-03-14 19:49:30 -0400

    tests/test_multiply_command.py file changes

--- tests/test_multiply_command.py
+++ tests/test_multiply_command.py
@@ -5,7 +5,7 @@ from app.plugins.multiply import MultiplyCommand
 
 # decorator is used to temporarily replace objects with mock objects during the execution of the test.
 @patch('builtins.input', side_effect=['2', '3'])
-@patch('calculator.Calculator.multiply', return_value=Decimal('6'))
+@patch('app.calculator.Calculator.multiply', return_value=Decimal('6'))
 def test_execute(mock_calculator_multiply, mock_input):
     '''Test execute function of MultiplyCommand.'''
     command = MultiplyCommand()
