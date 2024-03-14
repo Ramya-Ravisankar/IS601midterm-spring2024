2190433ce0515f7569a77e1e399fea811bb041c8
Ramya-Ravisankar <rb756@njit.edu>
2024-03-14 19:41:08 -0400

    tests/test_history.py file changes

--- tests/test_history.py
+++ tests/test_history.py
@@ -16,7 +16,7 @@ def test_add_calculation(setup_calculations):
     '''Tests that a calculation is added to history'''
     new_calc = calc.create_calculation(Decimal('2'), Decimal('2'), op.addition)
     his.add_calculation(new_calc)
-    assert his.get_lastest_history() == new_calc, "Failed to add the calculation to the history"
+    assert his.get_latest_history() == new_calc, "Failed to add the calculation to the history"
 
 def test_get_history(setup_calculations):
     '''Test retrieving the entire calculation history'''
@@ -30,10 +30,10 @@ def test_clear_history(setup_calculations):
 
 def test_get_latest_history(setup_calculations):
     '''Test getting the latest calculation from the history'''
-    latest = his.get_lastest_history()
+    latest = his.get_latest_history()
     assert latest.a == Decimal('20') and latest.b == Decimal('3'), "Did not get the correct latest calculation"
 
 def test_get_latest_history_with_empty_history():
     '''Test getting the latest calculation when the history is empty'''
     his.clear_history()
-    assert his.get_lastest_history() is None, "Expected None for latest calculation with empty history"
+    assert his.get_latest_history() is None, "Expected None for latest calculation with empty history"
