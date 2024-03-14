4a683b274af6df6c01eecc6b792661cd31c87b54
Ramya-Ravisankar <rb756@njit.edu>
2024-03-14 15:05:35 -0400

    app/plugins/multiply/__init__.py file changes

--- app/plugins/multiply/__init__.py
+++ app/plugins/multiply/__init__.py
@@ -2,6 +2,7 @@
 from app.commands import Command
 from calculator import Calculator
 from app.utils.validation import validate_decimal_input
+import logging
 
 class MultiplyCommand(Command):
     '''A command class to perform multiplication.'''
@@ -11,8 +12,11 @@ class MultiplyCommand(Command):
 
         This method prompts the user to enter two numbers and performs multiplication.
         '''
+        logging.info("Command 'multiply' from plugin 'menu' selected.")
         num1 = validate_decimal_input("Enter the first number: ")
         num2 = validate_decimal_input("Enter the second number: ")
+
+        logging.info("Performing multiplication...")
         result = Calculator.multiply(num1, num2)
         print(f"The result of {num1} * {num2} is: {result}")
         return result
