8ec8dfdd92aa08b907177af434ec700625df7155
Ramya-Ravisankar <rb756@njit.edu>
2024-03-29 22:32:31 -0400

    app/plugins/history/__init__.py file changes

--- app/plugins/history/__init__.py
+++ app/plugins/history/__init__.py
@@ -4,31 +4,42 @@ from calculator.calc_history import CalculationHistory
 import logging
 
 class HistoryCommand(Command):
-    '''A command class to manage calculation history'''
+    '''class to manage the history of calculations'''
 
-    def execute(self):
+    def execute(self,*args):
         '''Execute the HistoryCommand'''
 
-        logging.info("Command 'history' from plugin 'menu' selected.\n")
+    while True:
+        print ("\n --- History --- ")
         print("Choose an option:")
-        print("1. Retrieve the most recent calculation")
-        print("2. Retrieve all calculations so far")
-        print("3. Clear calculation history")
-        choice = input("Enter your choice: ")
+        print("1. Displays the most recent calculation done ")
+        print("2. Displays all calculations done so far")
+        print("3. Clears the calculation history")
+        print("4. Saves the history of calculations to the CSV")
+        print("5. Loads the history of calculations from the CSV")
+        print ("6. Deletes a calculation from history")
+        print("\nType 'exit' - returns back to the main menu.")
+        user_Option = input("\nEnter your choice: ")
 
-        if choice == '1':
-            self.retrieve_latest_calculation()
-        elif choice == '2':
-            self.retrieve_all_calculations()
-        elif choice == '3':
-            self.clear_history()
+            if user_Option.lower() == 'exit':
+                break  # Exit the loop, ending the command
+
+            self.process_choice(user_Option)
+
+    def process_choice(self, user_Option):
+        options = {
+            '1': self._display_most_recent_calculation,
+            '2': self._display_all_calculations,
+            '3': self._clear_history,
+            '4': self._save_calculation_history,
+            '5': self._load_calculation_history,
+            '6': self._delete_calculation_from_history,
+        }
+        action = options.get(user_Option)
+        if action:
+            action()
         else:
-            print("Invalid choice")
-
-    def retrieve_latest_calculation(self):
-        '''Retrieve the most recent calculation from the history and print its result'''
-        latest_calculation = CalculationHistory.get_latest_history()
-        self.print_result(latest_calculation)
+            print("Invalid choice. Select a valid option or type 'exit' to return to main menu.")
 
     def retrieve_all_calculations(self):
         '''Retrieve all calculations from the history and print their results'''
@@ -36,6 +47,11 @@ class HistoryCommand(Command):
         print("All Calculations:")
         for calculation in all_calculations:
             self.print_result(calculation)
+
+    def retrieve_latest_calculation(self):
+        '''Retrieve the most recent calculation from the history and print its result'''
+        latest_calculation = CalculationHistory.get_latest_history()
+        self.print_result(latest_calculation)
 
     def clear_history(self):
         '''Clear the calculation history'''
